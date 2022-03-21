e5e6e43df2f6aae382b0eb68eb3e6a8ac7304b72
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-21 18:02:20 +0300

    Add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[main].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
   module: {
     rules: [
       {
