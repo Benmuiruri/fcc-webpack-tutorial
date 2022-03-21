1d2da539d8b7db6431e002482135b88c9e3c3464
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-21 17:02:47 +0300

    Setup webpack bundling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-alert("Hello")
+import { run } from './app/app';
+import { AlertService } from './app/alert.service';
+import { ComponentService } from './app/component.service';
+
+const alertService = new AlertService();
+const componentService = new ComponentService();
+run(alertService, componentService);
