7db99da65158bc9a60213ed48a73edc1a4688d9b
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-21 17:42:09 +0300

    Add SASS loader to overight bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
@@ -9,8 +9,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       },
     ],
   },
